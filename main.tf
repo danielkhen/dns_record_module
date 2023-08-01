7db99f39b4bd9e67ae389ba3bf93b3d84504d335
daniel khen <danielkhen20@gmail.com>
2023-08-01 13:12:39 +0300

    Auto-Commit: bunch of cr fixes

--- main.tf
+++ main.tf
@@ -1,18 +1,5 @@
-resource "azurerm_dns_zone" "dns_zone" {
-  count = var.is_private ? 0 : 1
-
-  name                = var.dns_name
-  resource_group_name = var.resource_group_name
-
-  lifecycle {
-    ignore_changes = [tags["CreationDateTime"], tags["Environment"]]
-  }
-}
-
-resource "azurerm_private_dns_zone" "private_dns_zone" {
-  count = var.is_private ? 1 : 0
-
-  name                = var.dns_name
+resource "azurerm_private_dns_zone" "dns_zone" {
+  name                = var.name
   resource_group_name = var.resource_group_name
 
   lifecycle {
@@ -25,11 +12,11 @@ locals {
 }
 
 resource "azurerm_private_dns_zone_virtual_network_link" "vnet_links" {
-  for_each = var.is_private ? local.vnet_links_map : {}
+  for_each = local.vnet_links_map
 
   name                  = each.value.name
   resource_group_name   = var.resource_group_name
-  private_dns_zone_name = azurerm_private_dns_zone.private_dns_zone[0].name
+  private_dns_zone_name = azurerm_private_dns_zone.dns_zone.name
   virtual_network_id    = each.value.vnet_id
 
   lifecycle {
@@ -37,59 +24,36 @@ resource "azurerm_private_dns_zone_virtual_network_link" "vnet_links" {
   }
 }
 
+locals {
+  a_records_map = {for record in var.a_records: record.name => record}
+}
 
-resource "azurerm_dns_a_record" "a_record" {
-  count = !var.is_private && var.record_type == "a" ? 1 : 0
+resource "azurerm_private_dns_a_record" "records" {
+  for_each = local.a_records_map
 
-  name                = var.name
-  zone_name           = azurerm_dns_zone.dns_zone[0].name
-  resource_group_name = var.resource_group_name
-  ttl                 = var.ttl
-  records             = var.records
+  name                = each.value.name
+  resource_group_name = each.value.resource_group_name
+  zone_name           = azurerm_private_dns_zone.dns_zone.name
+  ttl                 = each.value.ttl
+  records             = each.value.records
 
   lifecycle {
     ignore_changes = [tags["CreationDateTime"], tags["Environment"]]
   }
 }
 
-
-resource "azurerm_private_dns_a_record" "private_a_record" {
-  count = var.is_private && var.record_type == "a" ? 1 : 0
-
-  name                = var.name
-  zone_name           = azurerm_private_dns_zone.private_dns_zone[0].name
-  resource_group_name = var.resource_group_name
-  ttl                 = var.ttl
-  records             = var.records
-
-  lifecycle {
-    ignore_changes = [tags["CreationDateTime"], tags["Environment"]]
-  }
+locals {
+  aaaa_records_map = {for record in var.aaaa_records: record.name => record}
 }
 
-resource "azurerm_dns_aaaa_record" "aaaa_record" {
-  count = !var.is_private && var.record_type == "aaaa" ? 1 : 0
+resource "azurerm_private_dns_aaaa_record" "records" {
+  count = local.aaaa_records_map
 
-  name                = var.name
-  zone_name           = azurerm_dns_zone.dns_zone[0].name
-  resource_group_name = var.resource_group_name
-  ttl                 = var.ttl
-  records             = var.records
-
-  lifecycle {
-    ignore_changes = [tags["CreationDateTime"], tags["Environment"]]
-  }
-}
-
-
-resource "azurerm_private_dns_aaaa_record" "private_aaaa_record" {
-  count = var.is_private && var.record_type == "aaaa" ? 1 : 0
-
-  name                = var.name
-  zone_name           = azurerm_private_dns_zone.private_dns_zone[0].name
-  resource_group_name = var.resource_group_name
-  ttl                 = var.ttl
-  records             = var.records
+  name                = each.value.name
+  resource_group_name = each.value.resource_group_name
+  zone_name           = azurerm_private_dns_zone.dns_zone.name
+  ttl                 = each.value.ttl
+  records             = each.value.records
 
   lifecycle {
     ignore_changes = [tags["CreationDateTime"], tags["Environment"]]
--- variables.tf
+++ variables.tf
@@ -1,5 +1,5 @@
 variable "name" {
-  description = "(Required) The name of the dns record."
+  description = "(Required) The name of the private dns record."
   type        = string
 }
 
@@ -8,43 +8,31 @@ variable "resource_group_name" {
   type        = string
 }
 
-variable "dns_name" {
-  description = "(Required) The name of the dns zone."
-  type        = string
-}
-
-variable "record_type" {
-  description = "(Required) The type of the dns record."
-  type        = string
-
-  validation {
-    condition     = contains(["a", "aaaa"], var.record_type)
-    error_message = "Current supported values for record type are a and aaaa."
-  }
-}
-
-variable "is_private" {
-  description = "(Optional) Is the dns zone private."
-  type        = bool
-  default     = true
-}
-
 variable "vnet_links" {
   description = "(Optional) A list of virtual networks to link with the dns zone, only needed when dns zone is private."
-  type = list(object({
+  type        = list(object({
     vnet_id = string
     name    = string
   }))
   default = []
 }
 
-variable "records" {
-  description = "(Required) List of ipv4 addresses."
-  type        = list(string)
+variable "a_records" {
+  description = "(Optional) The a records to add to the private dns zone."
+  type        = list(object({
+    name    = string
+    ttl     = number
+    records = list(string)
+  }))
+  default = []
 }
 
-variable "ttl" {
-  description = "(Required) The time to live of the dns record."
-  type        = number
-  default     = 300
+variable "aaaa_records" {
+  description = "(Optional) The aaaa records to add to the private dns zone."
+  type        = list(object({
+    name    = string
+    ttl     = number
+    records = list(string)
+  }))
+  default = []
 }
